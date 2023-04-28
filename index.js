1b55999978f5efac744817c7a9828c629f7ad87d
DaniyalJoonz <132060652+DaniyalJoonz@users.noreply.github.com>
2023-04-28 13:51:31 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('MTEwMTEyMjQwMzQ5ODcyOTUxMw.GSt-7y.tiPGZTS9A2cJLrTM9SBvirsNT8o8NcHnlZed5o');
